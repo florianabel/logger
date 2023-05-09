27dc74319b2cc47495019a127064c452b545f80a
floanwelt1 <floanwelt@gmail.com>
2023-05-09 17:52:08 -0500

    Added handler for logging; breaks stdout tests

--- __init__.py
+++ __init__.py
@@ -1,4 +1,13 @@
+import sys
+from datetime import datetime
 from threading import Lock
+from collections import namedtuple
+
+
+# Records
+Record = namedtuple("Record", "log_level msg")
+
+
 # Log level
 DEBUG = 10
 INFO = 20
@@ -19,6 +28,7 @@ _log_level_to_name = {
     ERROR: 'ERROR',
 }
 
+
 def _checkLogLevel(log_level):
     if isinstance(log_level, int):
         lv = log_level
@@ -32,6 +42,44 @@ def _checkLogLevel(log_level):
     return lv
 
 
+class Handler:
+    def __init__(self, log_level=DEBUG):
+        self.setLogLevel(log_level)
+
+    def setLogLevel(self, log_level):
+        self.log_level = _checkLogLevel(log_level)
+
+    def handle(self, record):
+        if record.log_level >= self.log_level:
+            prefix = self._prefix(record)
+            formatted_msg = f"{prefix} {record.msg}{self.terminator}"
+            self.emit(formatted_msg)
+
+    def emit(self, formatted_msg):
+        raise NotImplementedError(
+            'emit must be implemented by Handler subclasses')
+
+    def _prefix(self, record):
+        return f"[{self.HANDLER_PREFIX}][{_log_level_to_name[record.log_level]}][{datetime.now()}]"
+
+
+class ConsoleHandler(Handler):
+
+    terminator = '\n'
+    HANDLER_PREFIX = 'CONSOLE'
+
+    def __init__(self):
+        super().__init__()
+        self.stream = sys.stdout
+
+    def flush(self):
+        self.stream.flush()
+
+    def emit(self, formatted_msg):
+        self.stream.write(formatted_msg)
+        self.flush()
+
+
 class LoggerMeta(type):
     _instances = {}
 
@@ -43,11 +91,12 @@ class LoggerMeta(type):
                 instance = super().__call__(*args, **kwargs)
                 cls._instances[cls] = instance
             return cls._instances[cls]
-        
+
 
 class Logger(metaclass=LoggerMeta):
     def __init__(self, log_level=DEBUG):
         self.setLogLevel(log_level)
+        self.handlers = []
 
     def setLogLevel(self, log_level):
         self.log_level = _checkLogLevel(log_level)
@@ -56,21 +105,44 @@ class Logger(metaclass=LoggerMeta):
         isEnabled = log_level >= self.log_level
         return isEnabled
 
+    def basicConfig(self):
+        self.addConsoleHandler()
+
+    def addConsoleHandler(self):
+        handler = ConsoleHandler()
+        self.addHandler(handler)
+
+    def addHandler(self, handler):
+        if isinstance(handler, Handler):
+            if not handler in self.handlers:
+                self.handlers.append(handler)
+
     def debug(self, msg):
         if self.isEnabledFor(DEBUG):
-            print(msg)
+            self._log(DEBUG, msg)
 
     def info(self, msg):
         if self.isEnabledFor(INFO):
-            print(msg)
+            self._log(INFO, msg)
 
     def warning(self, msg):
         if self.isEnabledFor(WARNING):
-            print(msg)
+            self._log(WARNING, msg)
 
     def error(self, msg):
         if self.isEnabledFor(ERROR):
-            print(msg)
+            self._log(ERROR, msg)
+
+    def _log(self, level, msg):
+        if len(self.handlers) == 0:
+            self.basicConfig()
+        record = Record(level, msg)
+        self._handle(record)
+
+    def _handle(self, record):
+        for handler in self.handlers:
+            if record.log_level >= handler.log_level:
+                handler.handle(record)
 
 
 def getLogger():
