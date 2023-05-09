72d51eef480d59db86bf64fef65c6b958acf48f9
floanwelt1 <floanwelt@gmail.com>
2023-05-09 15:36:56 -0500

    Added support for setting log level by string

--- __init__.py
+++ __init__.py
@@ -5,6 +5,32 @@ INFO = 20
 WARNING = 30
 ERROR = 40
 
+_name_to_log_level = {
+    'DEBUG': DEBUG,
+    'INFO': INFO,
+    'WARNING': WARNING,
+    'ERROR': ERROR,
+}
+
+_log_level_to_name = {
+    DEBUG: 'DEBUG',
+    INFO: 'INFO',
+    WARNING: 'WARNING',
+    ERROR: 'ERROR',
+}
+
+def _checkLogLevel(log_level):
+    if isinstance(log_level, int):
+        lv = log_level
+    elif isinstance(log_level, str):
+        if log_level not in _name_to_log_level:
+            raise ValueError(f"Unknown log level: {log_level}")
+        lv = _name_to_log_level[log_level]
+    else:
+        raise TypeError(
+            f"Log level is not an integer or a valid string: {log_level}")
+    return lv
+
 
 class LoggerMeta(type):
     _instances = {}
@@ -21,10 +47,10 @@ class LoggerMeta(type):
 
 class Logger(metaclass=LoggerMeta):
     def __init__(self, log_level=DEBUG):
-        self.log_level = self.setLogLevel(log_level)
+        self.setLogLevel(log_level)
 
     def setLogLevel(self, log_level):
-        self.log_level = log_level
+        self.log_level = _checkLogLevel(log_level)
 
 
 def getLogger():
