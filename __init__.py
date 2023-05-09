60ccb16074bddf36062cff13ad838e11b3b18f5a
floanwelt1 <floanwelt@gmail.com>
2023-05-09 14:37:56 -0500

    Added Meta class and Logger Class

--- __init__.py
+++ __init__.py
@@ -1,5 +1,28 @@
+from threading import Lock
 # Log level
 DEBUG = 10
 INFO = 20
 WARNING = 30
 ERROR = 40
+
+
+class LoggerMeta(type):
+    _instances = {}
+
+    _lock: Lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:
+            if cls not in cls._instances:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
+            return cls._instances[cls]
+        
+
+class Logger(metaclass=LoggerMeta):
+    def __init__(self, log_level=DEBUG):
+        self.log_level = log_level
+
+
+def getLogger():
+    return Logger()
